c35fe9f89b13a707454e20b14361137abc264cda
TonkovKD <tonkovkd@inform.ivanovo.ru>
2026-07-03 16:27:53 +0300

    fix: remove todo

--- pet_ai/lib/theme/widgets/draggable_sheets/draggable_sheet.dart
+++ pet_ai/lib/theme/widgets/draggable_sheets/draggable_sheet.dart
@@ -5,7 +5,6 @@ class DragHandle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Center(
       child: Container(
         width: 40,
